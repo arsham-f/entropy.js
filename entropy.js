285e24dd3e70904239d8b7740d5640fa5e2fc477
Arsham <arsham.faghihi@gmail.com>
2014-02-19 16:20:00 -0500

    Refactor and isolate entropy.js

--- entropy.js
+++ entropy.js
@@ -1,13 +1,16 @@
-var counter = 0;
-var alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-var characters = alphanumeric.length;
-
-var key = "";
-$(document).mousemove(function (e) {
-	if (counter % 200 == 0) {
-		key = key + alphanumeric.substr((e.pageX * e.pageY) % characters, 1);
-		
-		$('#key').html("<b>[</b> " + key + " <b>]</b>");
-	}
-	counter++;
-});
+window.MouseEntropy = function(callback) {
+	var counter = 0,
+		alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+		characters = alphanumeric.length,
+		callback = callback || null,
+		key = "";
+	$(document).mousemove(function (e) {
+		if (counter % 100 == 0) {
+			key = key + alphanumeric.substr((e.pageX * e.pageY) % characters, 1);
+			
+			if (callback) 
+				callback.call(this, key);
+		}
+		counter++;
+	});
+}
